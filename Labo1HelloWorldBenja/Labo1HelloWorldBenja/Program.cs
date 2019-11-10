833cabf71419d703a5302604af32e89194267df0
Benjamin France <benjamin.france@student.hepl.be>
2019-11-10 14:27:02 +0100

    test

---
+++ Labo1HelloWorldBenja/Labo1HelloWorldBenja/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labo1HelloWorldBenja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Laboratoire numéro 1 de France Benjamin");
+            while (Console.Read() == 0) ;
+        }
+    }
+}
